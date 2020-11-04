72d58112c29483e4206025a271f2a130b79c68a1
Xin Guan <imbaguanxin@outlook.com>
2020-11-04 15:26:14 -0500

    update gitignore

--- zarr_tiff.py
+++ zarr_tiff.py
@@ -19,7 +19,7 @@ def get_ndarray_img_from_zarr(raw_file, raw_ds, coord_begin=None, coord_end=None
     cutout_ds = daisy.open_ds(raw_file, raw_ds)
     print(f'Voxel size: {cutout_ds}')
     roi = None
-    if coord_begin != None and coord_end != None:
+    if coord_begin is not None and coord_end is not None:
         voxel_size = cutout_ds.voxel_size
         coord_begin = Coordinate(np.flip(np.array(coord_begin))) * voxel_size
         coord_end = Coordinate(np.flip(np.array(coord_end))) * voxel_size
@@ -72,6 +72,10 @@ def down_sampling_img(input_img, scale_list):
 def write_tiff_to_zarr(ndarray, writer):
     raise NotImplementedError()
 
+def write_to_tiff(img, fpath):
+    tile = Image.fromarray(img)
+    tile.save(fpath, quality=95)
+
 ####################################################################
 
 
@@ -79,21 +83,53 @@ def main():
 
     raw_file = '/n/groups/htem/temcagt/datasets/cb2/zarr_volume/cb2_v3.zarr'
     raw_ds = "volumes/raw_mipmap/s2_rechunked"
-    output = '/n/groups/htem/temcagt/datasets/cb2/figure_section_tiffs/201004_data/small_roi_test'
-    coord_begin = [0, 0, 0]
-    coord_end= [62464, 57600, 1216]
+    # output = '/n/groups/htem/temcagt/datasets/cb2/figure_section_tiffs/201004_data/small_roi_test'
+    now = datetime.now().strftime("%m%d.%H.%M.%S")
+    output = f'/n/groups/htem/users/xg76/local_realignment/test_img/{now}'
+    # 3280-21435, 2840 22160, 70-1170
+    # start_row = start_col = 0
+    # coord_begin_row = 3280
+    # coord_begin_col = 2840
+    coord_begin = np.array([3280, 2840, 70])
+    coord_end= [21430, 22160, 71]#[18000, 18000, 1]#[62464, 57600, 1] # 1216
 
-    cutout_ds = daisy.open_ds(raw_file, raw_ds)
-    roi = calc_roi(cutout_ds.voxel_size, coord_begin, coord_end)
-    print(roi)
+    # cutout_ds = daisy.open_ds(raw_file, raw_ds)
+    # adder = 3000
+    # add_vec = np.array([3000, 3000, 1])
+    # final_img = np.zeros((18000, 18000))
+    # now_row = coord_begin_row
+    # pbar = tqdm(desc='GETTING IMG', total=36)
+    # for row in range(6):
+    #     now_col = coord_begin_col
+    #     for col in range(6):
+    #         coord_begin = [now_row, now_col, 70]
+    #         coord_end = [now_row + 3000, now_col + 3000, 71]
+    #         print(f'now coord: {coord_begin}')
+    #         roi = calc_roi(cutout_ds.voxel_size, coord_begin, coord_end)
+    #         print(f'now roi: {roi}')
+    #         ndarray = cutout_ds.to_ndarray(roi=roi)
+    #         print(ndarray.shape)
+    #         final_img[start_row: start_row+ 3000, start_col: start_col + 3000] = ndarray[0]
 
-    # for i in range(coord_end[2] - coord_begin[2]):
-    #     if i % 10 == 0:
+    #         start_col += 3000
+    #         now_col += 3000
+    #         pbar.update(1)
+    #     now_row += 3000
+    # pbar.close()
 
-
-    # raw_img_list = get_ndarray_img_from_zarr(raw_file, raw_ds, coord_begin, coord_end)
-
+    raw_img_list = get_ndarray_img_from_zarr(raw_file, raw_ds, coord_begin, coord_end)
+    # print(len(raw_img_list))
     # os.makedirs(output, exist_ok=True)
+    # write_to_tiff(final_img, os.path.join(output, '0_o.tif'))
+    # mipmap = down_sampling_img(final_img, [32])[0]
+    # write_to_tiff(mipmap, os.path.join(output, '0_m.tif'))
+    for i, img in enumerate(raw_img_list):
+        write_to_tiff(img, os.path.join(output, f'{i}_o.tif'))
+        mipmap = down_sampling_img(img, [32])[0]
+        write_to_tiff(mipmap, os.path.join(output, f'{i}_m.tif'))
+
+
+    os.makedirs(output, exist_ok=True)
 
     # pbar = tqdm(desc='WRITING TO TIFF', total=len(raw_img_list))
     # for idx, raw_img in enumerate(raw_img_list):
@@ -144,7 +180,7 @@ def test_mipmapping():
 
 if __name__ == "__main__":
     # print(cv2.__version__)
-    test_write_tiff()
-    # main()
+    # test_write_tiff()
+    main()
 
     # print(script_name)
